d26723bcda072fe9189fd78b7b4143f223b3cf2c
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2025-01-12 02:03:11 +0530

    refactor No Incognito test to use existing page instance instead of creating a new one

--- mytest/NoIncognitiTest.spec.ts
+++ mytest/NoIncognitiTest.spec.ts
@@ -2,7 +2,9 @@ import{test, Page, chromium, BrowserContext} from '@playwright/test';
 
 test("No Incognito test", async()=> {
     const browser:BrowserContext = await chromium.launchPersistentContext('', {headless:false});
-    const page:Page = await browser.newPage();
+    const Page = await browser.pages();
+    const page = Page[0];
+
 
     await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
 
